b46fe4e902763f9b87360715954fd06558b509f5
Nishantpindiproli <135137792+Nishantpindiproli@users.noreply.github.com>
2023-07-09 04:01:30 +0000

    homework 4

--- src/homework/06_tic_tac_toe/tic_tac_toe.h
+++ src/homework/06_tic_tac_toe/tic_tac_toe.h
@@ -1,1 +1,32 @@
 //h
+#include <string>
+#include <vector>
+#include <iostream>
+
+using std::string;
+using std::vector;
+using std::cout;
+using std::cin;
+
+#ifndef TIC_TAC_TOE_H
+#define TIC_TAC_TOE_H
+
+class TicTacToe
+{
+public: 
+    bool game_over();
+    void start_game(string first_player);
+    void mark_board(int position);
+    string get_player() const{return player;}
+    void display_board() const;
+
+private:
+    string player;
+    vector<string> pegs{9, " "};
+    void clear_board();
+    void set_next_player();
+    bool check_board_full();
+
+};
+
+#endif
