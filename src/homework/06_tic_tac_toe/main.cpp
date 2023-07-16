f01bbef077e014b7098a61b9711e98a6b2f383a2
Nishantpindiproli <135137792+Nishantpindiproli@users.noreply.github.com>
2023-07-16 03:13:44 +0000

    Hw 6 done

--- src/homework/06_tic_tac_toe/main.cpp
+++ src/homework/06_tic_tac_toe/main.cpp
@@ -1,47 +1,83 @@
 #include "tic_tac_toe.h"
+#include "tic_tac_toe_3.h"
+#include "tic_tac_toe_4.h"
+
+#include<iostream>
+#include<string>
+#include<vector>
+#include<memory>
+
+using std::cout; using std::cin; using std::string; using std::vector;
+using std::unique_ptr; using std::make_unique;
 
 int main() 
 {
-	TicTacToe game;
+	// TicTacToe game;
 	string first_player;
 	char user_choice = 'y';
+	unique_ptr<TicTacToe> game;
 
 	do
 	{
+
+		auto game_type = 0;
+
+		cout<<"Which size board, 3 or 4?";
+		cin>>game_type;
+
+
+	  	if(game_type == 3)
+		{
+			game = std::make_unique<TicTacToe3>();
+		}
+		else
+		{
+			game = std::make_unique<TicTacToe4>();
+		} 
+
 		cout<<"Enter first player: ";
+		
 		cin>>first_player;
 
 		if((first_player != "X") && (first_player != "O"))
 		{
 			do
 			{
-				cout<<"Please enter first player X or O: ";
+				cout<<"Please enter first player, X or O: ";
 				cin>>first_player;
 			}while((first_player != "X") && (first_player != "O"));
 		}
 
-		game.start_game(first_player);
+		game->start_game(first_player); // . changed to ->
 
 		int position;
 
-		while(!game.game_over())
+		while(!game->game_over()) // . changed to ->
 		{
-			cout<<"Enter a position: ";
+			cout<<"Enter a position, 1-";
+			if (game_type == 3)
+			{
+				cout<<"9";
+			}
+			else
+			{
+				cout<<"16";
+			}
 			cin>>position;
-			game.mark_board(position);
-			game.display_board();
+			game->mark_board(position); // . changed to ->
+			game->display_board();		// . changed to ->
 		}
 
-		if (game.get_winner() == "C")
+		if (game->get_winner() == "C") // . changed to ->
         {
-            cout<<"It's a tie!"<<"\n";
+            cout<<"It's a tie"<<"\n";
         }
         else
         {
-            cout<<"The Winner is: "<<game.get_winner()<<"\n";
+            cout<<"The winner is: "<<game->get_winner()<<"\n"; // . changed to ->
         }
 
-		cout<<"Play again? enter y or Y?";
+		cout<<"Play again? enter y or Y";
 		cin>>user_choice;
 
 	}while(user_choice == 'y' || user_choice == 'Y');
--- src/homework/06_tic_tac_toe/tic_tac_toe.cpp
+++ src/homework/06_tic_tac_toe/tic_tac_toe.cpp
@@ -1,4 +1,11 @@
+//cpp
 #include "tic_tac_toe.h"
+
+#include<iostream>
+#include<string>
+#include<vector>
+
+using std::cout; using std::cin; using std::string; using std::vector;
 
 bool TicTacToe::game_over()
 {
@@ -30,9 +37,13 @@ void TicTacToe::mark_board(int position)
 
 void TicTacToe::display_board() const
 {
-    for(long unsigned int i = 0; i < pegs.size(); i += 3)
+    for(long unsigned int i = 0; i < pegs.size(); i += std::sqrt(pegs.size()))
     {
-        cout<<pegs[i]<<"|"<<pegs[i+1]<<"|"<<pegs[i+2]<<"\n";
+        cout<<pegs[i]<<"|"<<pegs[i+1]<<"|"<<pegs[i+2];
+        
+        if(pegs.size() == 16) {cout<<"|"<<pegs[i+3];}
+
+        cout<<"\n";
     }
 }
 
@@ -73,41 +84,16 @@ bool TicTacToe::check_board_full() const
 
 bool TicTacToe::check_column_win()
 {
-    for (int i = 0; i < 3; i++)
-    {
-        if ((pegs[i] == pegs[i+3]) && (pegs[i+3] == pegs[i+6]) && (pegs[i] != " "))
-        {
-            return true;
-        }
-    }
-    
     return false;
 }
 
 bool TicTacToe::check_row_win()
 {
-    for (int i = 0; i < 9; i+=3)
-    {
-        if ((pegs[i] == pegs[i+1]) && (pegs[i+1] == pegs[i+2]) && (pegs[i] != " "))
-        {
-            return true;
-        }
-    }
-    
     return false;
 }
 
 bool TicTacToe::check_diagonal_win()
-{
-    if ((pegs[0] == pegs[4]) && (pegs[4] == pegs[8]) && (pegs[0] != " "))
-    {
-        return true;
-    }
-    else if ((pegs[6] == pegs[4]) && (pegs[4] == pegs[2]) && (pegs[6] != " "))
-    {
-        return true;
-    }
-    
+{ 
     return false;
 }
 
